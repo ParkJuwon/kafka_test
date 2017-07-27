f6798f21f28fae1b19e2b22ca94e4b09a3636f2c
john <john.p@kakaocorp.com>
2017-07-27 13:14:57 +0900

    init commit

---
+++ src/main/java/test/kafka/SimpleProducer.java
@@ -0,0 +1,8 @@
+package test.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+public class SimpleProducer {
+}
